1c3297210796ccc4190402379e4b0efc1178e8c8
Rob Hilgefort <rjhilgefort@gmail.com>
2015-11-08 15:48:47 -0500

    removed grunt-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.initConfig({
