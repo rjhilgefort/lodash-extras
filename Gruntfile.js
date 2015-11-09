3fc5ab39532a077fb4b53b5be6abda0282f85863
Rob Hilgefort <rjhilgefort@gmail.com>
2015-11-08 21:59:32 -0500

    add grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -17,14 +18,21 @@ module.exports = function (grunt) {
           ]
         },
         files: {
-          "./dist/lodash-extras.js": ["./src/index.js"]
+          "dist/lodash-extras.js": ["src/index.js"]
         }
       }
     },
 
-    uglify: {}
+    uglify: {},
+
+    watch: {
+      scripts: {
+        files: ["src/**/*.js"],
+        tasks: ["build"]
+      }
+    }
   });
 
   grunt.registerTask('build', ['browserify:dist']);
-  grunt.registerTask('default', ['build']);
+  grunt.registerTask('default', ['build', 'watch']);
 };
